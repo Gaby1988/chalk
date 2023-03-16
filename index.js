89a2195b729d2db90efe867ed9d3e4ec8e731b46
Gabriel Le_Rest <gabriellerest@gmail.com>
2023-03-16 10:09:46 +0100

    challllllllllllllkkkkkkkkkkkk

---
+++ index.js
@@ -0,0 +1,9 @@
+import chalk from 'chalk';
+const gabriel = "Gabriel";
+const olivier = "Olivier";
+const eric = "Eric";
+const cyril = "Cyril";
+console.log(chalk.blue(gabriel +" "));
+console.log(chalk.red(olivier +" "));
+console.log(chalk.green(eric +" "));
+console.log(chalk.yellow(cyril));
